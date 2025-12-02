f5a8716666a7befd24042af344d7497f5958e69a
Joe Robertson <joseph.robertson@nrel.gov>
2025-12-02 13:06:43 -0700

    Return before FT for halted workflow.

--- src/workflow/RunTranslator.cpp
+++ src/workflow/RunTranslator.cpp
@@ -33,6 +33,7 @@ void OSWorkflow::runTranslator() {
     LOG(Info, "Workflow halted, skipping OSM to IDF translation");
     // This allows model arguments to still be calculated
     workspace_ = Workspace{};
+    return;  // should this be before or after the line above?
   }
 
   auto runDir = workflowJSON.absoluteRunDir();
